af90b6e658563637c57a4d475b96ca52b1fb4362
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-10-29 07:07:54 +0900

    updated WcAuthService so that the token is stored in a member variable

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def main():
     intents = discord.Intents.default()
     intents.message_content = True
     intents.members = True
-      
     bot = commands.Bot(command_prefix="!", intents=intents)
     
     @bot.event
     async def on_ready():
         await bot.tree.sync()
         print("Bot ready and commands synced.")
+        
         
     @bot.event
     async def on_member_join(member): 
--- services/WcAuthService.py
+++ services/WcAuthService.py
@@ -4,18 +4,23 @@ import json
 import aiohttp, asyncio
 from aiohttp import BasicAuth
 class WcAuthService:
+    _access_token: str = None
+    
     async def getAccessToken(self):
-        client_id = os.getenv("WC_CLIENT_ID")
-        client_pwd = os.getenv("WC_CLIENT_PWD")
-        payload = {"grant_type": "client_credentials"}
-        auth = BasicAuth(client_id, client_pwd)
-        oauth_url = os.getenv("BASE_URL") + os.getenv("OAUTH_URI")
-        async with aiohttp.ClientSession() as session:
-            async with session.post(oauth_url, data=payload, auth=auth) as res:
-                if res.status == 200:
-                    json_data = await res.json()
-                    access_token = json_data.get("access_token")
-                    return access_token
-                else:
-                    print(f"Failed to retrieve access token. \n Status: {res.status()}")
-                    return None
+        if self._access_token is None:
+            client_id = os.getenv("WC_CLIENT_ID")
+            client_pwd = os.getenv("WC_CLIENT_PWD")
+            payload = {"grant_type": "client_credentials"}
+            auth = BasicAuth(client_id, client_pwd)
+            oauth_url = os.getenv("BASE_URL") + os.getenv("OAUTH_URI")
+            async with aiohttp.ClientSession() as session:
+                async with session.post(oauth_url, data=payload, auth=auth) as res:
+                    if res.status == 200:
+                        json_data = await res.json()
+                        access_token = json_data.get("access_token")
+                        return access_token
+                    else:
+                        print(f"Failed to retrieve access token. \n Status: {res.status()}")
+                        return None
+        else:
+            return self._access_token
